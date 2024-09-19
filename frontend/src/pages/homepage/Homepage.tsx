a22c2913a861bc548c7d1824167ccd330f3ff50f
Prosis <prosissestha8@gmail.com>
2024-09-19 21:23:33 +0545

    homepage

---
+++ frontend/src/pages/homepage/Homepage.tsx
@@ -0,0 +1,8 @@
+import { useEffect, useState } from "react";
+import styles from "./homepage.module.css";
+
+const Homepage = () => {
+  return <div className={styles.homepage_container}>Homepage</div>;
+};
+
+export default Homepage;
